5e25498b1d7b2515e4edb866ba3c71b4bb2aa98f
Alexander Hagerman <ahagerman@renci.org>
2022-01-05 16:51:56 -0500

    patch: remove key files until configured for local

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true,
-    https: {
-      key: fs.readFileSync('localhost-key.pem'),
-      cert: fs.readFileSync('localhost.pem'),
-    },
-  },
+  // server: {
+  //   open: true,
+  //   https: {
+  //     key: fs.readFileSync('localhost-key.pem'),
+  //     cert: fs.readFileSync('localhost.pem'),
+  //   },
+  // },
 });
