c670cb39f38f5fc487beacb0804dd39eb6e88134
Alexander Hagerman <ahagerman@renci.org>
2022-01-05 16:45:56 -0500

    patch: fix ci
    - Fix testing commands and setup CI runner

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
-  setupFilesAfterEnv: ['<rootDir>src/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   testPathIgnorePatterns: ['<rootDir>/cypress/'],
   moduleDirectories: ['node_modules', 'src'],
   collectCoverageFrom: ['src/**/*.{ts,tsx}', '!<rootDir>/node_modules/'],
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
+import fs from 'fs';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+    https: {
+      key: fs.readFileSync('localhost-key.pem'),
+      cert: fs.readFileSync('localhost.pem'),
+    },
+  },
 });
